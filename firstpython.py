fb4471fb33e46baefe03edde753ec85be881bbc4
Tute92 <89325965+Tute92@users.noreply.github.com>
2022-04-13 19:08:01 +0930

    Update firstpython.py

--- firstpython.py
+++ firstpython.py
@@ -1,2 +1,3 @@
 #Display the output
 print("New Python file")
+#check this link https://github.com/mring505/ds_capstone_MS/blob/master/predictive_analysis/SpaceX_Machine%20Learning%20Prediction_Part_5_MS.ipynb
